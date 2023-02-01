81417d57142ba1724c36d678981b0871babb3603
tadakazu1972 <tadakazu1972@gmail.com>
2023-02-01 22:48:13 +0900

    scaffold

--- app/src/main/java/com/example/lazygridtest/MainActivity.kt
+++ app/src/main/java/com/example/lazygridtest/MainActivity.kt
@@ -10,14 +10,11 @@ import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -44,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     LazyVerticalGrid(shuffledlist)
-                    Reset()
+                    //Reset()
                 }
             }
         }
@@ -78,7 +75,85 @@ fun LazyVerticalGrid( _list:List<String>){
     var count by remember { mutableStateOf(0) }
     var price: Int by remember { mutableStateOf(0)}
     val counter = rememberCounter()
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Open))
 
+    Scaffold(
+        scaffoldState = rememberScaffoldState(),
+        topBar = { Reset() },
+        bottomBar = {
+            Text(
+                text = "枚数: $count　 $price 円",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(start = 12.dp)
+                    .padding(end = 12.dp)
+                    .padding(bottom = 8.dp)
+            )
+        },
+    ){ padding ->
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            contentPadding = PaddingValues(
+                start = 12.dp,
+                top = 8.dp,
+                end = 12.dp,
+                bottom = 8.dp
+            ),
+            modifier = Modifier.fillMaxHeight()
+                .padding(top = 8.dp)
+                .padding(bottom = 48.dp)
+        ) {
+            /*item(span = { GridItemSpan(maxLineSpan) }) {
+                Surface(
+                    color = MaterialTheme.colors.primaryVariant,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "枚数 $count", modifier = Modifier.padding(8.dp))
+                }
+            }*/
+            items(_list.size) { index ->
+                var isClicked by rememberSaveable { mutableStateOf(false) }
+
+                Card(
+                    backgroundColor = Color.Red,
+                    modifier = Modifier.padding(4.dp).fillMaxWidth()
+                        .clickable {
+                            if (!isClicked) {
+                                count++
+                                isClicked = true
+                                price = count * 730
+                                counter.increment()
+                            }
+                        },
+                    elevation = 8.dp,
+                ) {
+                    if ( isClicked ){
+                        Text(
+                            text = _list[index],
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 30.sp,
+                            color = Color(0xFFFFFFFF),
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    } else {
+                        Text(
+                            text = "?",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 30.sp,
+                            color = Color(0xFFFFFFFF),
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    /*
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
         contentPadding = PaddingValues(
@@ -137,16 +212,17 @@ fun LazyVerticalGrid( _list:List<String>){
             }
         }
     }
-    Column{
+    Column {
         Text(
             text = "枚数: $count　 $price 円",
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth()
                 .padding(start = 12.dp)
-                .padding(end = 12.dp))
+                .padding(end = 12.dp)
+        )
     }
-
+    */
 }
 
 @Preview(showBackground = true)
